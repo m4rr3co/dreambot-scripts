5ef0f1bcbc8907b52fb7f9cd7fb7a7441ebf7d4c
m4rr3co <m4rr3co@hotmail.com>
2021-03-16 19:02:16 -0300

    GUI basics

--- GoTrade/GoTradeGUI.java
+++ GoTrade/GoTradeGUI.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 
 public class GoTradeGUI extends JFrame {
-
+    public GoTradeGUI() {
+        super("GoTrade - F2P Essentials");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
 }
