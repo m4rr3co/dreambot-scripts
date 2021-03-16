5e692efc068b7e183a5e566b19e199613cd5ee7f
m4rr3co <m4rr3co@hotmail.com>
2021-03-16 18:12:57 -0300

    Changes.

--- GoTrade/Main.java
+++ GoTrade/Main.java
@@ -1,6 +1,9 @@
 import framework.NodeBranch;
 import framework.TreeScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
 
+@ScriptManifest(category = Category.MISC, name = "GoTrade F2P", author = "m4rr3co", version = 0.1)
 public class Main extends TreeScript {
     @Override
     public NodeBranch getRootBranch() {
