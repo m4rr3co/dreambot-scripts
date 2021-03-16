d1ccf36c0938449fa609816facedb3c97d760e03
m4rr3co <m4rr3co@hotmail.com>
2021-03-16 18:43:14 -0300

    Adding gui class

--- GoTrade/Main.java
+++ GoTrade/Main.java
@@ -3,10 +3,20 @@ import framework.TreeScript;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
 
+import javax.swing.*;
+
 @ScriptManifest(category = Category.MISC, name = "GoTrade F2P", author = "m4rr3co", version = 0.1)
 public class Main extends TreeScript {
+    @Override
+    public void onStart() {
+        SwingUtilities.invokeLater(() ->{
+
+        });
+    }
+
     @Override
     public NodeBranch getRootBranch() {
         return null;
     }
+
 }
