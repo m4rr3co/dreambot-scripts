d41fc635bea4a36bba62a3e679cebc42ae32e250
m4rr3co <m4rr3co@hotmail.com>
2021-03-16 19:07:06 -0300

    GUI basics

--- GoTrade/GoTradeGUI.java
+++ GoTrade/GoTradeGUI.java
@@ -1,15 +1,19 @@
 import org.dreambot.api.methods.MethodProvider;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GoTradeGUI extends JFrame {
     public GoTradeGUI() {
         super("GoTrade - F2P Essentials");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        initComponents();
+        add(initComponents());
         setVisible(true);
     }
-    private void initComponents() {
+    private JPanel initComponents() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setPreferredSize(new Dimension(200,200));
         MethodProvider.log("Initiating GUI components");
+        return mainPanel;
     }
 }
