70423b399797e10edbb129a481ef52517dac3328
m4rr3co <m4rr3co@hotmail.com>
2021-03-16 17:39:26 -0300

    Commit: Testing github versioning

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-public class Main {
+import framework.NodeBranch;
+import framework.TreeScript;
+
+public class Main extends TreeScript {
+    @Override
+    public NodeBranch getRootBranch() {
+        return null;
+    }
 }
