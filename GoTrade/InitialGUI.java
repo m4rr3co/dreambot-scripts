5a056dc0c5df07141fb52d2f3cfc710954ce0b8d
m4rr3co <m4rr3co@hotmail.com>
2021-03-17 10:53:02 -0300

    JScrollpane added

--- GoTrade/InitialGUI.java
+++ GoTrade/InitialGUI.java
@@ -5,22 +5,29 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class initialGUI extends JFrame {
+public class InitialGUI extends JFrame {
+
+    //Testing GUI look and feel psvm
+    public static void main(String[] args) {
+        new InitialGUI(new ArrayList<>());
+    }
 
     //List for storing tasks chosen by user
     private ArrayList<NodeBranch> tasks;
     //List for storing all implemented tasks
-    private ArrayList<NodeBranch> availableTasks;
+    private JList<NodeBranch> availableTasks;
 
     /**
      * @param tasks List of tasks to be done in this session
      */
-    public initialGUI(ArrayList<NodeBranch> tasks) {
+    public InitialGUI(ArrayList<NodeBranch> tasks) {
         super("GoTrade - F2P Essentials");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         this.tasks = tasks;
         setAvailableTasks();
         add(initComponents());
+        pack();
+        setResizable(false);
         setVisible(true);
     }
 
@@ -31,7 +38,11 @@ public class initialGUI extends JFrame {
     private JPanel initComponents() {
         JPanel mainPanel = new JPanel();
         mainPanel.setPreferredSize(new Dimension(200,200));
-        MethodProvider.log("Initiating GUI components");
+
+        JScrollPane tasksMenu = new JScrollPane(availableTasks);
+        tasksMenu.setBorder(BorderFactory.createTitledBorder("Available Tasks"));
+        tasksMenu.setPreferredSize(new Dimension(200,200));
+        mainPanel.add(tasksMenu);
         return mainPanel;
     }
     private void setAvailableTasks() {
--- GoTrade/Main.java
+++ GoTrade/Main.java
@@ -11,7 +11,7 @@ public class Main extends TreeScript {
     private ArrayList<NodeBranch> tasks = new ArrayList<>();
     @Override
     public void onStart() {
-        SwingUtilities.invokeLater(() -> new initialGUI(tasks));
+        SwingUtilities.invokeLater(() -> new InitialGUI(tasks));
     }
 
     @Override
